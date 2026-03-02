70ebc259196a0b38f12ebc8727b420e1892530fb
globalyoya <cr5436@srmist.edu.in>
2026-03-02 09:40:08 +0530

    initial commit on uc2

---
+++ OOPSbannerapp2.java
@@ -0,0 +1,11 @@
+public class OOPSbannerapp2(){
+	public static void main(string args[]){
+		system.out.println("   ***   "+"   ***   "+"*******   "+"  *******");
+		system.out.println(" **   ** "+" **   ** "+"**     ** "+"**       ");
+		system.out.println(" **   ** "+" **   ** "+"**     ** "+"**       ");
+		system.out.println(" **   ** "+" **   ** "+"**     ** "+"  ****** ");
+		system.out.println(" **   ** "+" **   ** "+"*******   "+"       **");
+		system.out.println(" **   ** "+" **   ** "+"**        "+"       **");
+		system.out.println("   ***   "+"   ***   "+"**        "+"  ****** ");
+	}
+}
