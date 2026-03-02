449a7b1328c6ef5831429e11d430b56792387019
globalyoya <cr5436@srmist.edu.in>
2026-03-02 22:36:24 +0530

    Add new feature files

---
+++ OOPSbannerapp3.java
@@ -0,0 +1,11 @@
+public class OOPSbannerapp3{
+	public static void main(String args[]){
+		System.out.println(String.join("","   ***   ","   ***   ","*******   ","  *******"));
+		System.out.println(String.join(""," **   ** "+" **   ** "+"**     ** "+"**       "));
+		System.out.println(String.join(""," **   ** "+" **   ** "+"**     ** "+"**       "));
+		System.out.println(String.join(""," **   ** "+" **   ** "+"**     ** "+"  ****** "));
+		System.out.println(String.join(""," **   ** "+" **   ** "+"*******   "+"       **"));
+		System.out.println(String.join(""," **   ** "+" **   ** "+"**        "+"       **"));
+		System.out.println(String.join("","   ***   "+"   ***   "+"**        "+"  ****** "));
+	}
+}
